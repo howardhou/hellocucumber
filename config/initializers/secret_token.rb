4db5ae8ad979e389268bf8a5db03068fa83ddf6f
howardhou <houdd321@hotmail.com>
2013-08-27 13:45:55 +0800

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellocucumber::Application.config.secret_token = 'e4919d8aa3c629f68abff43e6a682c90d2a6134b9f334259e69183c649b1b7a57203b65bb48b10502e6d98de821de041cb008f28b8d0b377dd9aaab1256d2b50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hellocucumber::Application.config.session_store :cookie_store, key: '_hellocucumber_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hellocucumber::Application.config.session_store :active_record_store
